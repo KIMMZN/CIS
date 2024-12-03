1cf85952b7de28e1aedc307e873071a626b2d074
J1NU2 <pj1nu2@gmail.com>
2024-12-03 17:35:26 +0900

    Add : 근태 목록 페이징 Service Interface 추가 241129

--- src/main/java/com/cis/attendance/service/IF_AttendanceService.java
+++ src/main/java/com/cis/attendance/service/IF_AttendanceService.java
@@ -6,5 +6,6 @@ import java.util.List;
 
 public interface IF_AttendanceService {
     public void workStart(AttendanceDTO attendanceDTO) throws Exception;
-    public List<AttendanceDTO> attendanceList() throws Exception;
+    public List<AttendanceDTO> attendanceList(int startIndex, int pageSize) throws Exception;
+    public int attendanceListCnt() throws Exception;
 }
