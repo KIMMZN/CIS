06758571ca0fda98d72faf8d99b0e808bb6d9f3f
J1NU2 <pj1nu2@gmail.com>
2024-12-03 17:27:58 +0900

    Add : 출근 기능 Controller 추가 241129

--- src/main/java/com/cis/attendance/controller/AttendanceController.java
+++ src/main/java/com/cis/attendance/controller/AttendanceController.java
@@ -1,21 +1,28 @@
 package com.cis.attendance.controller;
 
+import com.cis.attendance.dto.AttendanceDTO;
+import com.cis.attendance.service.IF_AttendanceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AttendanceController {
+
+    @Autowired
+    private IF_AttendanceService attendanceService;
 
     @GetMapping(value = "attendance")
     public String attendance(Model model) {
         return "attendance/attendance";
     }
 
-    @PostMapping(value = "work_start")
-    public String workStart(Model model) {
-        System.out.println("work_start Controller");
+    @PostMapping(value = "go_to_work")
+    public String workStart(@ModelAttribute AttendanceDTO attendanceDTO, Model model) throws Exception {
+        attendanceService.workStart(attendanceDTO);
         return "redirect:attendance";
     }
 }
