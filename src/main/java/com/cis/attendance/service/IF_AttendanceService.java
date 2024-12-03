db634eed310da9b4499dcddc01e06d4483a75037
J1NU2 <pj1nu2@gmail.com>
2024-12-03 17:49:32 +0900

    Add : 정시퇴근 기능 Service Interface 추가 241129

--- src/main/java/com/cis/attendance/service/IF_AttendanceService.java
+++ src/main/java/com/cis/attendance/service/IF_AttendanceService.java
@@ -7,5 +7,6 @@ import java.util.List;
 public interface IF_AttendanceService {
     public void workStart(AttendanceDTO attendanceDTO) throws Exception;
     public List<AttendanceDTO> attendanceList(int startIndex, int pageSize) throws Exception;
+    public void attendanceMod(AttendanceDTO attendanceDTO) throws Exception;
     public int attendanceListCnt() throws Exception;
 }
