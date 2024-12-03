257662292fba4f13c0639b74b64cd219e5f55580
J1NU2 <pj1nu2@gmail.com>
2024-12-03 17:32:07 +0900

    Add : 출근 목록 조회 Service Interface 추가 241129

--- src/main/java/com/cis/attendance/service/IF_AttendanceService.java
+++ src/main/java/com/cis/attendance/service/IF_AttendanceService.java
@@ -2,6 +2,9 @@ package com.cis.attendance.service;
 
 import com.cis.attendance.dto.AttendanceDTO;
 
+import java.util.List;
+
 public interface IF_AttendanceService {
     public void workStart(AttendanceDTO attendanceDTO) throws Exception;
+    public List<AttendanceDTO> attendanceList() throws Exception;
 }
