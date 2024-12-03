eb58498d09a5c7983794453ad421e08af8d5a4bc
J1NU2 <pj1nu2@gmail.com>
2024-12-03 17:28:57 +0900

    Add : 출근 기능 Service 추가 241129

--- src/main/java/com/cis/attendance/service/AttendanceService.java
+++ src/main/java/com/cis/attendance/service/AttendanceService.java
@@ -1,8 +1,19 @@
 package com.cis.attendance.service;
 
+import com.cis.attendance.dto.AttendanceDTO;
+import com.cis.attendance.repository.IF_AttendanceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AttendanceService implements IF_AttendanceService {
+
+    @Autowired
+    IF_AttendanceRepository attendanceRepository;
+
+    @Override
+    public void workStart(AttendanceDTO attendanceDTO) throws Exception {
+        attendanceRepository.attendanceInsert(attendanceDTO);
+    }
 
 }
