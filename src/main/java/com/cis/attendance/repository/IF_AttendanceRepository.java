e1e255ad1001628ebb656ea0657111873b935da2
J1NU2 <pj1nu2@gmail.com>
2024-12-03 17:32:42 +0900

    Add : 출근 목록 조회 Repository Interface 추가 241129

--- src/main/java/com/cis/attendance/repository/IF_AttendanceRepository.java
+++ src/main/java/com/cis/attendance/repository/IF_AttendanceRepository.java
@@ -4,8 +4,11 @@ import com.cis.attendance.dto.AttendanceDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @Mapper
 public interface IF_AttendanceRepository {
     public void attendanceInsert(AttendanceDTO attendanceDTO) throws Exception;
+    public List<AttendanceDTO> attendanceSelectAll() throws Exception;
 }
