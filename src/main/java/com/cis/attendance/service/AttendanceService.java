719eec2481fb791a31d25428925d536821a6d6c3
J1NU2 <pj1nu2@gmail.com>
2024-12-03 17:32:20 +0900

    Add : 출근 목록 조회 Service 추가 241129

--- src/main/java/com/cis/attendance/service/AttendanceService.java
+++ src/main/java/com/cis/attendance/service/AttendanceService.java
@@ -5,6 +5,8 @@ import com.cis.attendance.repository.IF_AttendanceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AttendanceService implements IF_AttendanceService {
 
@@ -14,6 +16,11 @@ public class AttendanceService implements IF_AttendanceService {
     @Override
     public void workStart(AttendanceDTO attendanceDTO) throws Exception {
         attendanceRepository.attendanceInsert(attendanceDTO);
+    }
+
+    @Override
+    public List<AttendanceDTO> attendanceList() throws Exception {
+        return attendanceRepository.attendanceSelectAll();
     }
 
 }
