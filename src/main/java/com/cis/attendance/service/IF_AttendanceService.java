6f4ef7bd71a9a11ea3c88de699107b7201e12bd6
J1NU2 <pj1nu2@gmail.com>
2024-12-03 17:28:35 +0900

    Add : 출근 기능 Service Interface 추가 241129

--- src/main/java/com/cis/attendance/service/IF_AttendanceService.java
+++ src/main/java/com/cis/attendance/service/IF_AttendanceService.java
@@ -1,5 +1,7 @@
 package com.cis.attendance.service;
 
-public interface IF_AttendanceService {
+import com.cis.attendance.dto.AttendanceDTO;
 
+public interface IF_AttendanceService {
+    public void workStart(AttendanceDTO attendanceDTO) throws Exception;
 }
