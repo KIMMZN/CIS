926791783ee4e2f4b8d07de5b96a9cc86aa324dc
J1NU2 <pj1nu2@gmail.com>
2024-12-10 10:17:15 +0900

    Mod : 메인화면 직원 Session 연결 Controller 수정 241204

--- src/main/java/com/cis/main/controller/MainController.java
+++ src/main/java/com/cis/main/controller/MainController.java
@@ -2,6 +2,7 @@ package com.cis.main.controller;
 
 import com.cis.attendance.dto.AttendanceDTO;
 import com.cis.attendance.service.IF_AttendanceService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,8 +17,11 @@ public class MainController {
     private IF_AttendanceService attendanceService;
 
     @GetMapping(value = "emp_main")
-    public String empMain(Model model) throws Exception {
-        List<AttendanceDTO> attendance_list = attendanceService.attendanceList(0, 3);
+    public String empMain(Model model, HttpSession httpSession) throws Exception {
+        Object login_emp = httpSession.getAttribute("employee_id");
+        if (login_emp == null) return "total_login";
+
+        List<AttendanceDTO> attendance_list = attendanceService.attendanceList(login_emp, 0, 3);
         model.addAttribute("attendance_list", attendance_list);
         return "main/emp_main";
     }
