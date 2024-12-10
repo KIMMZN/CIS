aa725671ce75faa10cca9c143a0d11f637e44850
J1NU2 <pj1nu2@gmail.com>
2024-12-10 10:49:27 +0900

    Mod : 근태관리 Controller 코드 정리 및 수정 241206

--- src/main/java/com/cis/attendance/controller/AttendanceController.java
+++ src/main/java/com/cis/attendance/controller/AttendanceController.java
@@ -20,45 +20,54 @@ public class AttendanceController {
     @Autowired
     private IF_AttendanceService attendanceService;
 
+    // 근태관리 목록 조회, 근태관리 페이지 이동
     @GetMapping(value = "attendance")
-    public String attendance(@RequestParam(defaultValue = "1") int page, Model model, HttpSession httpSession) throws Exception {
+    public String attendance(@RequestParam(defaultValue = "1") int page,
+                             Model model,
+                             HttpSession httpSession) throws Exception {
         Object login_emp = httpSession.getAttribute("employee_id");
         if (login_emp == null) return "total_login";
 
         int totalListCnt = attendanceService.attendanceListCnt(login_emp);
-        Pagination pagination = new Pagination(6, totalListCnt, page);
 
-        int startIndex = pagination.getStartIndex();
-        int pageSize = pagination.getPageSize();
+        Pagination pagination = new Pagination(6, totalListCnt, page);
         pagination.setSelectPage(page);
 
-        List<AttendanceDTO> attendance_list = attendanceService.attendanceList(login_emp, startIndex, pageSize);
+        List<AttendanceDTO> attendance_list = attendanceService.attendanceList(login_emp, pagination);
 
         model.addAttribute("attendance_list", attendance_list);
         model.addAttribute("pagination", pagination);
+
         return "attendance/attendance";
     }
 
+    // 출근
     @PostMapping(value = "go_to_work")
-    public String workStart(@ModelAttribute AttendanceDTO attendanceDTO, HttpSession httpSession) throws Exception {
+    public String workStart(@ModelAttribute AttendanceDTO attendanceDTO,
+                            HttpSession httpSession) throws Exception {
         Object login_emp = httpSession.getAttribute("employee_id");
         if (login_emp == null) return "total_login";
 
         attendanceDTO.setEmp_id((String)login_emp);
 
         attendanceService.workStart(attendanceDTO);
+
         return "redirect:attendance";
     }
 
+    // 퇴근
     @PostMapping(value = "leave_work")
-    public String workEnd(@ModelAttribute AttendanceDTO attendanceDTO, HttpSession httpSession) throws Exception {
+    public String workEnd(@ModelAttribute AttendanceDTO attendanceDTO,
+                          HttpSession httpSession) throws Exception {
         Object login_emp = httpSession.getAttribute("employee_id");
         if (login_emp == null) return "total_login";
 
         attendanceDTO.setEmp_id((String)login_emp);
 
         if (attendanceDTO.getSignificant().equals("퇴근")) attendanceDTO.setSignificant(null);
+
         attendanceService.attendanceMod(attendanceDTO);
+
         return "redirect:attendance";
     }
 
