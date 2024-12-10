239155494937998bef02fa63978e249f5f9f4488
J1NU2 <pj1nu2@gmail.com>
2024-12-10 10:11:41 +0900

    Mod : 이메일 직원 Session 연결 Controller 수정 241204

--- src/main/java/com/cis/email/controller/EmailController.java
+++ src/main/java/com/cis/email/controller/EmailController.java
@@ -5,6 +5,7 @@ import com.cis.Pagination;
 import com.cis.email.dto.EmailDTO;
 import com.cis.email.dto.EmailFileDTO;
 import com.cis.email.service.IF_EmailService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,8 +22,11 @@ public class EmailController {
     private EmailFileUtils emailFileUtils;
 
     @GetMapping(value = "email")
-    public String email(@RequestParam(value = "filter", defaultValue = "all") String filter, @RequestParam(defaultValue = "1") int page , Model model) throws Exception {
-        int totalListCnt = emailservice.emailListCnt(filter);
+    public String email(@RequestParam(value = "filter", defaultValue = "all") String filter, @RequestParam(defaultValue = "1") int page, Model model, HttpSession httpSession) throws Exception {
+        Object login_emp = httpSession.getAttribute("employee_id");
+        if (login_emp == null) return "total_login";
+
+        int totalListCnt = emailservice.emailListCnt(login_emp, filter);
         Pagination pagination = new Pagination(6, totalListCnt, page);
 
         int startIndex = pagination.getStartIndex();
@@ -30,7 +34,7 @@ public class EmailController {
         pagination.setSelectPage(page);
         pagination.setFilter(filter);
 
-        List<EmailDTO> email_list = emailservice.emailList(startIndex, pageSize, filter);
+        List<EmailDTO> email_list = emailservice.emailList(login_emp, startIndex, pageSize, filter);
 
         model.addAttribute("email_list", email_list);
         model.addAttribute("pagination", pagination);
@@ -38,25 +42,33 @@ public class EmailController {
     }
 
     @PostMapping(value = "email_delete")
-    public String emailDelete(@RequestParam("num") List<String> email_num_list) throws Exception {
-        for (int i = 0; i < email_num_list.size(); i++) {
-            String email_num = email_num_list.get(i);
+    public String emailDelete(@RequestParam("num") List<String> email_num_list, HttpSession httpSession) throws Exception {
+        Object login_emp = httpSession.getAttribute("employee_id");
+        if (login_emp == null) return "total_login";
+
+        for (String email_num : email_num_list) {
             List<EmailFileDTO> file_name_list = emailservice.emailNumFind(email_num);
             emailFileUtils.deleteFiles(file_name_list);
-            emailservice.emailDelete(email_num);
+            emailservice.emailDelete(login_emp, email_num);
         }
         return "redirect:email";
     }
 
     @GetMapping(value = "email_send")
-    public String mailSend() {
+    public String mailSend(HttpSession httpSession) {
+        if ((httpSession.getAttribute("employee_id")) == null) return "total_login";
         return "email/mail_send";
     }
 
     @PostMapping(value = "email_insert")
-    public String mailInsert(@ModelAttribute EmailDTO emaildto) throws Exception {
+    public String mailInsert(@ModelAttribute EmailDTO emaildto, HttpSession httpSession) throws Exception {
+        Object login_emp = httpSession.getAttribute("employee_id");
+        if (login_emp == null) return "total_login";
+
+        emaildto.setSender_id((String)login_emp);
         emailservice.emailInsert(emaildto);
-        String mail_num = emailservice.emailOrderOne();
+
+        String mail_num = emailservice.emailOrderOne(login_emp);
         List<EmailFileDTO> email_files = emailFileUtils.uploadFiles(emaildto.getMail_files());
         emailservice.emailFileUpload(mail_num, email_files);
         return "redirect:email";
@@ -72,10 +84,13 @@ public class EmailController {
     }
 
     @GetMapping(value = "email_detail")
-    public String mailDetail(@RequestParam("num") String email_num, Model model) throws Exception {
-        EmailDTO email_one = emailservice.emailOne(email_num);
+    public String mailDetail(@RequestParam("num") String email_num, Model model, HttpSession httpSession) throws Exception {
+        Object login_emp = httpSession.getAttribute("employee_id");
+        if (login_emp == null) return "total_login";
+
+        EmailDTO email_one = emailservice.emailOne(login_emp, email_num);
         List<EmailFileDTO> email_files = emailservice.emailNumFind(email_num);
-        if (email_one.getMail_check().equals("N")) emailservice.emailCheckUpdate(email_num);
+        if (email_one.getMail_check().equals("N")) emailservice.emailCheckUpdate(login_emp, email_num);
         model.addAttribute("email_files", email_files);
         model.addAttribute("email_one", email_one);
         return "email/mail_detail";
